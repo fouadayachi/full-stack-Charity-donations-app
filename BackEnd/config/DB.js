70a225cfbde2f01681ba83241a3241ed59dcd6cf
fouadayachi <ayachifouad20@gmail.com>
2025-04-07 13:03:33 +0100

    update V5

--- BackEnd/config/DB.js
+++ BackEnd/config/DB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URL2);
+        const connect = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB connected: ${connect.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);

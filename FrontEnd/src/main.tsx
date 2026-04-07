3b4a98826a8a06eb96e7997c0c39154810c76bfa
fouadayachi <ayachifouad20@gmail.com>
2026-04-07 19:33:32 +0100

    update

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "@heroui/react/styles.css"; // or
-import "@heroui/theme/styles.css";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
